89207e6b54993cd9c1e379e3f689913efcb13868
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-08-23 21:00:53 +0000

    I put images in their own folder

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setTimeout(function(){
-    while(1) {
-      window.open("https://free-minecraft.micahmaphet.repl.co/", "", "width=100, height=100, left=" + Math.random() * screen.width + ", top=" + Math.random() * screen.height);
-      window.open("https://free-minecraft.micahmaphet.repl.co/");
-      location.reload(1);
-    }
+    // while(1) {
+    //   window.open("https://free-minecraft.micahmaphet.repl.co/", "", "width=100, height=100, left=" + Math.random() * screen.width + ", top=" + Math.random() * screen.height);
+    //   window.open("https://free-minecraft.micahmaphet.repl.co/");
+    //   location.reload(1);
+    // }
   }, 1000);
